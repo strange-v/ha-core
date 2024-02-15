0a1e26f1e47b51397f7e12384eb2266aa0b300b6
strange_v <strange.rand@gmail.com>
2024-02-15 19:30:08 +0000

    Refactoring and adding support of new node types and sensors

--- homeassistant/components/rfm_gateway/__init__.py
+++ homeassistant/components/rfm_gateway/__init__.py
@@ -1,14 +1,16 @@
 """The RFM Gateway component."""
+# Registering devices https://developers.home-assistant.io/docs/device_registry_index/
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import device_registry as dr
-from homeassistant.helpers.typing import ConfigType
 
-from .const import CONF_GATEWAYS, DOMAIN, MACUFACTURER
+from .const import DOMAIN
 
-PLATFORMS = [Platform.SENSOR]
+PLATFORMS = [
+    Platform.BINARY_SENSOR,
+    Platform.SENSOR,
+]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -16,23 +18,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
-    device_registry = dr.async_get(hass)
-    for gateway in entry.data[CONF_GATEWAYS]:
-        device_registry.async_get_or_create(
-            config_entry_id=entry.entry_id,
-            identifiers={(DOMAIN, gateway["mac"])},
-            manufacturer=MACUFACTURER,
-            name=gateway["name"],
-        )
-
     # Forward the setup to the sensor platform.
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
     return True
 
 
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up the custom component from yaml configuration."""
-    hass.data.setdefault(DOMAIN, {})
-    return True
+# async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+#     """Set up the custom component from yaml configuration."""
+#     hass.data.setdefault(DOMAIN, {})
+#     return True
